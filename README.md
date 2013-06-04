5ca209a0aa61a3f16845ce662c9b9532a26bbd2a
Chris Brousseau <brousseau.chris@gmail.com>
2013-06-03 20:00:42 -0400

    committing libTTS, license, samples

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
 libTTS
 ======
 
-A portable and simple text-to-speech library powered by Google Translate's Text-To-Speech service.
+libTTS is portable and simple text-to-speech library powered by Google Translate's Text-To-Speech service.
+
+[Google Translate](http://translate.google.com/) already provides a full-fledged text-to-speech service for dynamically creating short text-to-speech files.  However, due to restrictions of the service, it is not possible to play these files on external websites; a request to the text-to-speech API will give you a 404.  More information on these restrictions can be found [here](http://stackoverflow.com/questions/12883330/request-to-google-text-to-speech-api).
+
+This library encapsulates an `<iframe>` that loads the Google Translate Text-To-Speech url directly.
+
+Examples
+--------
+
+Please see sample.html for examples
+
+Use
+---
+
+libTTS creates a global variable `TTS`.  Please check you namespaces!
+
+### TTS.say()
+
+`say(text)` - pass a string to the text-to-speech engine.
+
+````javascript
+    TTS.say("Hello, World!");
+    // [TTS] Said: "Hello, World!"
+````
+
+Successfully spoken phrases will be printed to the console.  Calling `TTS.say()` while another phrase is being spoken will abort the previous audio playback.  There is a currently a *100-character limit* on the text string (imposed by Google).  Text strings that exceed this limit will print a warning in the console.
+
+### TTS.maxLength
+
+integer - the allowed max length of a string passed to `TTS.say()`
+
+````javascript
+    TTS.maxLength;
+    // 100
+````
+
+Please use 'TTS.maxLength` for validation checks against string lengths before passing them to `TTS.say()`
+
+### TTS.el
+
+jQuery element - the iframe element that plays the audio.
+
+````javascript
+    TTS.el;
+    // jQuery <iframe> element
+````
+
+
+Dependencies
+------------
+
+libTTS currently requires [jQuery](http://jquery.com/).
---
+++ tts.js
@@ -0,0 +1,104 @@
+/**
+ * Google Text-To-Speach Library
+ * @version 0.0.1
+ * @author Chris Brousseau
+ * Utilizes the google text to speach (tts) "api" on any page using an iframe
+ * Google logic prohibits loading the externally generated MP3 files via normal Audio() object.
+ */
+
+;
+// setup global singleton TTS namespace
+var TTS;
+(function($){
+
+	TTS = {
+		/**
+		 * @var el
+		 * jQuery element for iframe that we will load google in
+		 */
+		el: null,
+		
+		/**
+		 * @var urlTpl string
+		 * the url of the google translate, with a %s for input tts string
+		 */
+		urlTpl: "http://translate.google.com/translate_tts?tl=en&q=%s",
+		
+		/**
+		 * @var maxLength int
+		 * Google's max character length of the input tts string
+		 */
+		maxLength: 100, // google TTS limit
+		
+		/**
+		 * init()
+		 * sets up the iframe we'll load the google TTS into
+		 */
+		init: function(){
+			this.el = $(document.createElement('iframe'));
+			this.el.attr({
+				src: 'javascript:false'
+			}).css({
+				display: 'none'
+			});
+			$("body").append(this.el);
+		},
+		
+		/**
+		 * createUrl()
+		 * @param text string
+		 * creates a url using the urlTpl and the text string
+		 */
+		createUrl: function(text){
+			return this.urlTpl.replace("%s",encodeURIComponent(text));
+		},
+		
+		/**
+		 * standardizeText()
+		 * @param text string
+		 * There are certain optimizations we can do to maximize the amount of text we can send to google
+		 */
+		standardizeText: function(text){
+			return text.trim()
+			.replace(/\s\s+/, ' '); // replace multiple spaces with only one
+		},
+		
+		/**
+		 * say()
+		 * @param text string
+		 * public method for providing a text string to send to google TTS
+		 */
+		say: function(text){
+			text = this.standardizeText(text);
+			if(text.length > this.maxLength){
+				console.warn("[TTS] string too long to be read: \"" + text + "\"");
+				/**
+				 * @TODO split up strings and read one-by-one?
+				 * is there any way to monitor the playback in the iframe to know when it's complete?
+				 * (It doesn't look likely)
+				 */
+			} else {
+				this._say(text);
+			}
+		},
+		/**
+		 * _say()
+		 * @param text string
+		 * internal method for sending text string to google, in case we find a way to batch long strings
+		 */
+		_say: function(text){
+			if(text){
+				console.log("[TTS] Said: \"" + text + "\"");
+				this.el.attr({
+					src: this.createUrl(text)
+				});
+			}
+		}
+	};
+
+	// setup iframe on document ready
+	$(document).ready(function(){
+		TTS.init();
+	});
+
+})(jQuery);
